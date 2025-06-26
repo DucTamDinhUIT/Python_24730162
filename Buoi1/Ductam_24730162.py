bc4ca72e1bfbd397f8f59c219b92a8f2c2e42708
DucTamDinhUIT <ductam.astro@gmail.com>
2025-06-26 09:13:25 +0700

    buổi 1

--- Buoi1/Ductam_24730162.py
+++ Buoi1/Ductam_24730162.py
@@ -1,2 +1,14 @@
 a = input("nhap du lieu: ")
 print(type(a))
+# bai tap 3
+
+s1 = input("nhap string 1: ")
+s2 = input("nhap string 2: ")
+s3 = s1 + s2
+print(s3)
+print(s1,s2)
+
+#3. Viết chương trình nhập vào hai string. Sau đó dùng “,” hoặc “+” trong print() để kết hai string đó và xuất ra màn hình.
+#4. Viết lệnh nhập thông tin từ bàn phím, rồi xuất ra giá trị nhập, viết lệnh input() lồng trong lệnh print().
+
+print(input())
